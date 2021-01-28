fdfe8f38242dccf52e5a541e5415528b0efd2776
Aaron Utterback <Autterback3@mail.greenriver.edu>
2021-01-28 15:39:35 -0500

    added meal routes and updated controller

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <?php
+// CONTROLLER
 // turn on error reporting
 ini_set('display_error', 1);
 error_reporting(E_ALL);
+
+// Start a session
+session_start();
 
 // require the autoload file
 require_once('vendor/autoload.php');
@@ -16,20 +20,41 @@ $f3->route('GET /' , function(){
     echo $view->render('/views/home.html');
 });
 
-// Run fat free
-$f3->run();
-
 // Define an order route
 $f3->route('GET /order' , function(){
-    echo "Order Route";
+    $view = new Template();
+    echo $view->render('/views/form1.html');
 });
 
 // Define an order2 route
-$f3->route('GET /order2' , function(){
-    echo "Order 2 Route";
+$f3->route('POST /order2' , function(){
+    // var_dump($_POST); // first page getting data
+    if(isset($_POST['food'])){
+        $_SESSION['food'] = $_POST['food'];
+    }
+    if(isset($_POST['meal'])){
+        $_SESSION['meal'] = $_POST['meal'];
+    }
+    //Display a view
+    $view = new Template();
+    echo $view->render('/views/form2.html');
 });
 
 // Define an order route
-$f3->route('GET /summary' , function(){
-    echo "summary";
+$f3->route('POST /summary' , function(){
+    //echo "<p>POST:</p>";
+    //var_dump($_POST);
+
+    //echo "<p>SESSION:</p>";
+    //var_dump($_SESSION);
+
+    if(isset($_POST['conds'])){
+        $_SESSION['conds'] = implode(", ",$_POST['conds']);
+    }
+
+    $view = new Template();
+    echo $view->render('/views/summary.html');
 });
+
+// Run fat free
+$f3->run();
